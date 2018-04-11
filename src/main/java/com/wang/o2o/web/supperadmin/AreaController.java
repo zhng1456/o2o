012314c15e0a113a3df3f50a4cf6c163d6c5c157
zhng1456 <410814938@qq.com>
2018-04-11 15:45:53 +0800

    add logback

--- src/main/java/com/wang/o2o/web/supperadmin/AreaController.java
+++ src/main/java/com/wang/o2o/web/supperadmin/AreaController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +19,15 @@ import com.wang.o2o.service.AreaService;
 @Controller
 @RequestMapping("/supperadmin")
 public class AreaController {
+	Logger logger=LoggerFactory.getLogger(AreaController.class);
 	@Autowired
 	private AreaService areaService;
 	@RequestMapping(value="/listarea",method=RequestMethod.GET)
 	//自动转化为json返回前端 
 	@ResponseBody
 	private Map<String,Object> listArea(){
+		logger.info("=====start=======");
+		long startTime=System.currentTimeMillis();
 		Map<String,Object> modelMap=new HashMap<String,Object>();
 		List<Area> list=new ArrayList<Area>();
 		try{
@@ -35,7 +40,10 @@ public class AreaController {
 			modelMap.put("success",false);
 			modelMap.put("errMsg",e.toString());
 		}
+		logger.error("test error!");
+		long endTime=System.currentTimeMillis();
+		logger.debug("cost time:[{}ms]",endTime-startTime);
+		logger.info("==end==");
 		return modelMap;
-		
 	}
 }
